cc7df8dee285f2ce8a80d79b76f79e900e3ed231
phoebe <phoebelmurphy@gmail.com>
2012-02-23 14:47:55 +0000

    f this

---
+++ src/phm1/NewJ/RunNewJ.java
@@ -0,0 +1,14 @@
+package phm1.NewJ;
+
+public class RunNewJ {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		MainFrame frame = new MainFrame();
+		frame.setVisible(true);
+
+	}
+
+}
