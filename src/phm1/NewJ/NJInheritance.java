c1e99ef591bf4d5eb38ed9c53fe389bbd222b528
Joe MacMahon <joe@losingthegame.net>
2012-03-08 12:55:52 +0000

    Work on arrows getting there. I fucking hate everything.

--- src/phm1/NewJ/NJInheritance.java
+++ src/phm1/NewJ/NJInheritance.java
@@ -12,8 +12,29 @@ public class NJInheritance extends NJConnection {
 	
 	public void draw (Graphics g, NJClass from){
 		super.draw(g, from);
-		if(from != null){
-			
+		int vertexAX, vertexAY, vertexBX, vertexBY, vertexCX, vertexCY;
+		vertexAX = vertexAY = vertexBX = vertexBY = vertexCX = vertexCY = 0;
+		
+		// TODO For fuck's sake put all of this shit in java.awt.Point objects... - J
+		if(this.getTo() != null){
+			//TODO
+		} else {
+			if(this.isHorizontal()){
+				// Vertical
+				if(this.getyTo() < this.getyFrom()){
+					vertexAX = this.getxTo();
+					vertexAY = this.getTo().getY() + this.getTo().getB();
+					
+					vertexBX = this.getxTo() + 5;
+					vertexBY = this.getTo().getY() + this.getTo().getB() + 10;
+					
+					vertexCX = this.getxTo() - 5;
+					vertexCY = this.getTo().getY() + this.getTo().getB() + 10;
+				}
+			}
 		}
+		g.drawLine(vertexAX, vertexAY, vertexBX, vertexBY);
+		g.drawLine(vertexBX, vertexBY, vertexCX, vertexCY);
+		g.drawLine(vertexAX, vertexAY, vertexCX, vertexCY);
 	}
 }
